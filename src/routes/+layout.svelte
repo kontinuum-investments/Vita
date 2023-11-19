69cc101d233fc196c02fcc2e4b426d5bd6cde904
Kavindu Athaudha <kavindu@kih.com.sg>
2023-11-20 10:21:08 +1300

    Initial Commit.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script lang='ts'>
+    import '../app.pcss';
+    import { NavBar } from 'aorta-arcturus';
+
+</script>
+
+<NavBar />
+
+<slot></slot>
--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    import {NavBar} from 'aorta-arcturus';
-</script>
-
-<NavBar/>
